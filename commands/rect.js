9f0d963f9fa0a07d2a53aad21270fd9c878ea8bc
sushant-c <sushantadhikari2001@gmail.com>
2021-12-28 15:24:38 +0545

    finalized

--- commands/rect.js
+++ commands/rect.js
@@ -3,7 +3,8 @@ const { MessageAttachment } = require("discord.js");
 
 module.exports = {
   name: "r",
-  description: "This is the command to show only the rectangle for showing datas!",
+  description:
+    "This is the command to show only the rectangle for showing datas!",
   async execute(message, args) {
     const canvas = Canvas.createCanvas(900, 100);
     const ctx = canvas.getContext("2d");
@@ -19,18 +20,18 @@ module.exports = {
 
     const marginLR = 10;
     const marginTop = 35;
-    const spaceForField = canvas.width/8; // 8 is the number of field to include
+    const spaceForField = canvas.width / 8; // 8 is the number of field to include
 
     ctx.font = "28px sans-serif";
     ctx.fillStyle = "#ffffff";
     ctx.fillText("Symbol", marginLR, marginTop);
-    ctx.fillText("Open", spaceForField + marginLR, marginTop)
-    ctx.fillText("High", spaceForField*2 , marginTop)
-    ctx.fillText("Low", spaceForField*3 - 2*marginLR, marginTop)
-    ctx.fillText("Traded", spaceForField*4 - 5*marginLR, marginTop)
-    ctx.fillText("LTP", spaceForField*5 - 3*marginLR, marginTop)
-    ctx.fillText("Change", spaceForField*6 - 6*marginLR, marginTop)
-    ctx.fillText("Previous", spaceForField*7 - 3*marginLR, marginTop)
+    ctx.fillText("Open", spaceForField + marginLR, marginTop);
+    ctx.fillText("High", spaceForField * 2 - marginLR, marginTop);
+    ctx.fillText("Low", spaceForField * 3 - 4 * marginLR, marginTop);
+    ctx.fillText("Traded", spaceForField * 4 - 7 * marginLR, marginTop);
+    ctx.fillText("LTP", spaceForField * 5 - 7 * marginLR, marginTop);
+    ctx.fillText("Change (%)", spaceForField * 6 - 10 * marginLR, marginTop);
+    ctx.fillText("Change Pt.", spaceForField * 7 - 4 * marginLR, marginTop);
 
     const attachment = new MessageAttachment(
       canvas.toBuffer(),
